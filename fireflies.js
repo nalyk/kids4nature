f94b034ae88c52c1587b7d14f728599fbfeec9a9
nalyk <dev.ungheni@gmail.com>
2025-10-16 13:49:37 +0300

    feat: make fireflies animation more dreamy

--- fireflies.js
+++ fireflies.js
@@ -1,4 +1,3 @@
-
 const canvas = document.getElementById('fireflies-canvas');
 const ctx = canvas.getContext('2d');
 
@@ -8,15 +7,18 @@ canvas.height = window.innerHeight;
 const fireflies = [];
 const numFireflies = 100;
 
+const colors = ['#86EFAC', '#22C55E', '#FDE68A'];
+
 class Firefly {
     constructor() {
         this.x = Math.random() * canvas.width;
         this.y = Math.random() * canvas.height;
         this.size = Math.random() * 2 + 1;
-        this.speedX = Math.random() * 2 - 1;
-        this.speedY = Math.random() * 2 - 1;
+        this.speedX = Math.random() * 0.5 - 0.25;
+        this.speedY = Math.random() * 0.5 - 0.25;
         this.opacity = Math.random();
-        this.flicker = Math.random() * 0.02;
+        this.flicker = Math.random() * 0.01;
+        this.color = colors[Math.floor(Math.random() * colors.length)];
     }
 
     update() {
@@ -39,7 +41,10 @@ class Firefly {
     draw() {
         ctx.beginPath();
         ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2);
-        ctx.fillStyle = `rgba(255, 255, 255, ${this.opacity})`;
+        ctx.fillStyle = this.color;
+        ctx.globalAlpha = this.opacity;
+        ctx.shadowColor = this.color;
+        ctx.shadowBlur = 10;
         ctx.fill();
     }
 }
@@ -52,6 +57,7 @@ function init() {
 
 function animate() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.filter = 'blur(2px)';
 
     for (const firefly of fireflies) {
         firefly.update();
